a5d2ffef63f09dcce7e97692e5c9e88397395017
Cayo Legal <cayolegal@Cayos-MacBook-Air.local>
2022-04-19 19:27:01 -0400

    introduction TS

---
+++ ejercicios/typescript/index.ts
@@ -0,0 +1,10 @@
+console.log("Hello world")
+
+function add (a: number, b: number):number {
+
+    return a + b
+}
+
+const sum = add(2,3)
+
+//Comando en la terminal si no esta cargando el index.html de la carpeta de ts rm -rf .cache dist
